c2381debf34219a770391bcbc4cd6b22debc6ca2
Md Mamunur Rashid <mmncit@gmail.com>
2020-01-02 14:00:52 -0700

    concurrently download online images

--- threads/download-images-concurrent.py
+++ threads/download-images-concurrent.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import concurrent.futures
 
 img_urls = [
     'https://images.unsplash.com/photo-1516117172878-fd2c41f4a759',
@@ -21,8 +22,9 @@ img_urls = [
 
 t1 = time.perf_counter()
 
+
 # download images
-for img_url in img_urls:
+def download_image(img_url):
     img_bytes = requests.get(img_url).content
     img_name = img_url.split('/')[3]
     img_name = f'{img_name}.jpg'
@@ -30,6 +32,9 @@ for img_url in img_urls:
         img_file.write(img_bytes)
         print(f'{img_name} was downloaded...')
 
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(download_image, img_urls)
 
 t2 = time.perf_counter()
 
